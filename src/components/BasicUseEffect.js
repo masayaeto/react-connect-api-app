f610244436c8e530d72c0e77fd60b2c8edfe7c46
衞藤勝也 <masaya@etomasauyanoMacBook-Pro.local>
2021-02-24 07:48:27 +0900

    複数要素に対してのuseEffectの使い方

---
+++ src/components/BasicUseEffect.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react'
+
+const BasicUseEffect = () => {
+  const [count, setCount] = useState(0)
+  const [item, setitem] = useState("")
+  useEffect(() => {
+    console.log("useEffect invoked")
+  },[count])
+  return (
+    <div>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>Click {count}</button>
+      <input type="text" value={item} onChange={evt => setitem(evt.target.value)}/>
+    </div>
+  )
+}
+
+export default BasicUseEffect
